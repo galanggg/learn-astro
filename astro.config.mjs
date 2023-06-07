f33c5b76e405b182f657fed354ceec9fbefab536
galanggg <tugasgalang@gmail.com>
2023-06-07 20:21:08 +0800

    feat: add new adapter for deploy the web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
+
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.app',
 	output: 'hybrid',
-	experimental: {
-		hybridOutput: true,
-	},
 	integrations: [react(), solidJs()],
+	adapter: netlify(),
 });
